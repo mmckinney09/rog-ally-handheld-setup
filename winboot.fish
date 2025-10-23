2ec9283e8fd9d2ede47bc2000a6dfa4a2ad4f551
mmckinney09 <35412418+mmckinney09@users.noreply.github.com>
2025-10-23 09:44:29 -0400

    Update winboot.fish

--- winboot.fish
+++ winboot.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 # Reboot into Windows 11 from CachyOS
 # Usage: Run this from Steam
-#Replace 0003 with your actual Windows boot number
+# Replace 0003 with your actual Windows boot number
 
 sudo efibootmgr --bootnext 0003
 sudo reboot
